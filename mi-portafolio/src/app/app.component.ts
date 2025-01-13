635bb5b4821a107bb5debe7fd8367ed0f0d1676e
jspq <juansebastianpq812@gmail.com>
2025-01-13 15:33:15 -0500

    🚧 Se añade componentes nav y footer. Se agrega configuración TailWind CSS

--- mi-portafolio/src/app/app.component.ts
+++ mi-portafolio/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MiPortafolioComponent } from './components/mi-portafolio/mi-portafolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MiPortafolioComponent,
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ mi-portafolio/src/app/components/mi-portafolio/mi-portafolio.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'mi-portafolio',
+  standalone: true,
+  imports: [NavComponent,
+    FooterComponent
+  ],
+  templateUrl: './mi-portafolio.component.html',
+  styleUrl: './mi-portafolio.component.css'
+})
+export class MiPortafolioComponent {
+
+}
